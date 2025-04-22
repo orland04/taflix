2b13785430eda9615298706dcc1c81edb150318c
orland04 <72548287+orland04@users.noreply.github.com>
2025-04-22 10:14:01 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'ca55e3d4f64f5b56cf3ce5a7b181fea9';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
